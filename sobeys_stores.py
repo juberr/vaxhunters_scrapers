1df1d5422f4fe821620b254ea4d9f01f60250f32
juberr <juberr1@live.ca>
2021-05-05 22:40:01 -0400

    created function from mkds API POC for specific area code

--- sobeys_stores.py
+++ sobeys_stores.py
@@ -1,63 +1,75 @@
 import requests
 import json
 import csv
+from key import API_KEY
 
-stores = {}
+def get_locations_postalcode(postalcode):
+  stores = {}
+  pcode = postalcode.upper()
 
-with open("./canadacities.csv", "r") as file:
-  reader = csv.reader(file)
-  for row in reader:
-    if(row[0]) == 'city':
-      continue
 
-    ### stores around certain location - gets their external id, which we need for the appointments request
-    url = "https://api.pharmacyappointments.ca/public/locations/search"
+  # get info from google maps API
+  gurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+  g_params = {
+    'key':API_KEY,
+    'address': pcode
+  }
+  # get JSON
+  ginfo = requests.get(gurl, params=g_params).json()
 
-    payload = json.dumps({
-      "location": {
-        "lat": float(row[4]),
-        "lng": float(row[5])
-      },
-      "fromDate": "2021-05-01",
-      "vaccineData": "WyJhM3A1bzAwMDAwMDAwVzdBQUkiLCJhM3A1bzAwMDAwMDAwVzJBQUkiLCJhM3A1bzAwMDAwMDAwVzNBQUkiLCJhM3A1bzAwMDAwMDAwVzVBQUkiLCJhM3A1bzAwMDAwMDAwV1dBQVkiLCJhM3A1bzAwMDAwMDAwZjRBQUEiLCJhM3A1bzAwMDAwMDAwZk9BQVEiLCJhM3A1bzAwMDAwMDAwZllBQVEiLCJhM3A1bzAwMDAwMDAwZ2xBQUEiLCJhM3A1bzAwMDAwMDAwbnJBQUEiLCJhM3A1bzAwMDAwMDAwZkpBQVEiXQ==",
-      "locationQuery": {
-        "includePools": [
-          "default"
-        ]
-      },
-      "doseNumber": 1,
-      "url": "https://www.pharmacyappointments.ca/location-select"
-    })
-    headers = {
-      'accept': 'application/json, text/plain, */*',
-      'accept-encoding': 'gzip, deflate, br',
-      'accept-language': 'en-US,en;q=0.9',
-      'content-length': '537',
-      'content-type': 'application/json',
-      'origin': 'https://www.pharmacyappointments.ca',
-      'referer': 'https://www.pharmacyappointments.ca/',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-site',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-      'x-correlation-id': '4d2a6fae-6f0c-4ff0-bca0-58bbd03ec9fb'
-    }
+  # parse JSON
+  lat = ginfo['results'][0]['geometry']['location']['lat']
+  lng = ginfo['results'][0]['geometry']['location']['lng']
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+ #stores around certain location - gets their external id, which we need for the appointments request
+  url = "https://api.pharmacyappointments.ca/public/locations/search"
 
-    locations = json.loads(response.text)["locations"]
+  payload = json.dumps({
+    "location": {
+      "lat": lat,
+      "lng": lng
+    },
+    "fromDate": "2021-05-01",
+    "vaccineData": "WyJhM3A1bzAwMDAwMDAwVzdBQUkiLCJhM3A1bzAwMDAwMDAwVzJBQUkiLCJhM3A1bzAwMDAwMDAwVzNBQUkiLCJhM3A1bzAwMDAwMDAwVzVBQUkiLCJhM3A1bzAwMDAwMDAwV1dBQVkiLCJhM3A1bzAwMDAwMDAwZjRBQUEiLCJhM3A1bzAwMDAwMDAwZk9BQVEiLCJhM3A1bzAwMDAwMDAwZllBQVEiLCJhM3A1bzAwMDAwMDAwZ2xBQUEiLCJhM3A1bzAwMDAwMDAwbnJBQUEiLCJhM3A1bzAwMDAwMDAwZkpBQVEiXQ==",
+    "locationQuery": {
+      "includePools": [
+        "default"
+      ]
+    },
+    "doseNumber": 1,
+    "url": "https://www.pharmacyappointments.ca/location-select"
+  })
+  headers = {
+    'accept': 'application/json, text/plain, */*',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-length': '537',
+    'content-type': 'application/json',
+    'origin': 'https://www.pharmacyappointments.ca',
+    'referer': 'https://www.pharmacyappointments.ca/',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'x-correlation-id': '4d2a6fae-6f0c-4ff0-bca0-58bbd03ec9fb'
+  }
 
-    for location in locations:
-      if location["name"] not in stores.keys():
-        object = {
-          "displayAddress": location["displayAddress"],
-          "extId": location["extId"],
-          "regionExternalId": location["regionExternalId"],
-          "location": location["location"],
-          "type": location["type"],
-          "timezone": location["timezone"]
+  response = requests.request("POST", url, headers=headers, data=payload)
 
-        }
-        stores[ location["name"] ] = object
+  locations = json.loads(response.text)["locations"]
 
-with open('sobeys_stores.json', 'w') as f:
-    json.dump(stores, f, indent=4)
+  for location in locations:
+    if location["name"] not in stores.keys():
+      object = {
+        "displayAddress": location["displayAddress"],
+        "extId": location["extId"],
+        "regionExternalId": location["regionExternalId"],
+        "location": location["location"],
+        "type": location["type"],
+        "timezone": location["timezone"]
+
+      }
+      stores[ location["name"] ] = object
+
+  return locations
+
+print(get_locations('L1N4J6'))
